879adea6b90c21b158e67ee0662161152a957f2f
Dyalt <57507092+DyaltFrozyl@users.noreply.github.com>
2021-05-26 12:13:11 -0500

    Add files via upload

--- interface/scripted/ss_acidbarrel/ss_acidbarrelgui.lua
+++ interface/scripted/ss_acidbarrel/ss_acidbarrelgui.lua
@@ -2,119 +2,132 @@ require "/scripts/util.lua"
 require "/scripts/interp.lua"
 
 function init()
-  self.itemList = "itemScrollArea.itemList"
-
-  self.upgradeLevel = config.getParameter("upgradeLevel")
-
-  self.upgradeableWeaponItems = {}
-  self.selectedItem = nil
+  itemList = "itemScrollArea.itemList"
+	
+  defaultCost = config.getParameter("defaultCost", 0)
+  upgradeTag = config.getParameter("upgradeTag")
+	sb.logInfo(upgradeTag)
+  upgradeItem = config.getParameter("upgradeItem")
+	
+  self.upgradeableItems = {}
+  selectedItem = nil
   populateItemList()
+	
+	repopulateTimer = 1
+	
+	animation = config.getParameter("animation")
+	animationTimer = 0
 end
 
 function update(dt)
-  populateItemList()
+	repopulateTimer = math.max(0, repopulateTimer - dt)
+	if repopulateTimer == 0 then
+		itemSelected()
+		populateItemList()
+		repopulateTimer = 1
+	end
+	
+	if animation then
+		animationTimer = (animationTimer + dt / animation.time) % 1
+		local frame = math.floor(animationTimer * animation.frames)
+		local image = animation.image..":"..frame
+		widget.setImage("animatedImage", image)
+	end
 end
 
 function upgradeCost(itemConfig)
   if itemConfig == nil then return 0 end
-
-  local prevValue = root.evalFunction("weaponEssenceValue", itemConfig.parameters.level or itemConfig.config.level or 1)
-  local newValue = root.evalFunction("weaponEssenceValue", self.upgradeLevel)
-
-  return math.floor(newValue - prevValue)
+	return math.floor(itemConfig.config.upgradeCost or defaultCost)
 end
 
 function populateItemList(forceRepop)
-  local upgradeableWeaponItems = player.itemsWithTag("upgradeableWeapon")
-  for i = 1, #upgradeableWeaponItems do
-    upgradeableWeaponItems[i].count = 1
+  local upgradeableItems = player.itemsWithTag(upgradeTag)
+  for i = 1, #upgradeableItems do
+    upgradeableItems[i].count = 1
   end
 
-  local playerEssence = player.currency("essence")
-
-  if forceRepop or not compare(upgradeableWeaponItems, self.upgradeableWeaponItems) then
-    self.upgradeableWeaponItems = upgradeableWeaponItems
-    widget.clearListItems(self.itemList)
+  if forceRepop or not compare(upgradeableItems, self.upgradeableItems) then
+    self.upgradeableItems = upgradeableItems
+    widget.clearListItems(itemList)
     widget.setButtonEnabled("btnUpgrade", false)
 
     local showEmptyLabel = true
 
-    for i, item in pairs(self.upgradeableWeaponItems) do
+    for i, item in pairs(self.upgradeableItems) do
       local config = root.itemConfig(item)
+			
+			showEmptyLabel = false
 
-      if (config.parameters.level or config.config.level or 1) < self.upgradeLevel then
-        showEmptyLabel = false
+			local listItem = string.format("%s.%s", itemList, widget.addListItem(itemList))
+			local name = config.config.shortdescription
 
-        local listItem = string.format("%s.%s", self.itemList, widget.addListItem(self.itemList))
-        local name = config.config.shortdescription
+			widget.setText(string.format("%s.itemName", listItem), name)
+			widget.setItemSlotItem(string.format("%s.itemIcon", listItem), item)
 
-        widget.setText(string.format("%s.itemName", listItem), name)
-        widget.setItemSlotItem(string.format("%s.itemIcon", listItem), item)
-
-        local price = upgradeCost(config)
-        widget.setData(listItem,
-          {
-            index = i,
-            price = price
-          })
-
-        widget.setVisible(string.format("%s.unavailableoverlay", listItem), price > playerEssence)
-      end
+			local price = upgradeCost(config)
+			widget.setData(listItem, {index = i, price = price})
+			
+			local available = player.hasCountOfItem({name = upgradeItem})
+			widget.setVisible(string.format("%s.unavailableoverlay", listItem), available < price)
     end
 
-    self.selectedItem = nil
-    showWeapon(nil)
+    selectedItem = nil
+    showItem(nil)
 
     widget.setVisible("emptyLabel", showEmptyLabel)
   end
 end
 
-function showWeapon(item, price)
-  local playerEssence = player.currency("essence")
+function showItem(item, price)
+	local available = player.hasCountOfItem({name = upgradeItem})
   local enableButton = false
-
+	
   if item then
-    enableButton = playerEssence >= price
+    enableButton = available >= price
     local directive = enableButton and "^green;" or "^red;"
-    widget.setText("essenceCost", string.format("%s%s / %s", directive, playerEssence, price))
+    widget.setText("scourgeCost", string.format("%s%s / %s", directive, available, price))
   else
-    widget.setText("essenceCost", string.format("%s / --", playerEssence))
+    widget.setText("scourgeCost", string.format("%s / --", available))
   end
 
   widget.setButtonEnabled("btnUpgrade", enableButton)
 end
 
 function itemSelected()
-  local listItem = widget.getListSelected(self.itemList)
-  self.selectedItem = listItem
+  local listItem = widget.getListSelected(itemList)
+  selectedItem = listItem
 
   if listItem then
-    local itemData = widget.getData(string.format("%s.%s", self.itemList, listItem))
-    local weaponItem = self.upgradeableWeaponItems[itemData.index]
-    showWeapon(weaponItem, itemData.price)
+    local itemData = widget.getData(string.format("%s.%s", itemList, listItem))
+    local item = self.upgradeableItems[itemData.index]
+    showItem(item, itemData.price)
   end
 end
 
 function doUpgrade()
-  if self.selectedItem then
-    local selectedData = widget.getData(string.format("%s.%s", self.itemList, self.selectedItem))
-    local upgradeItem = self.upgradeableWeaponItems[selectedData.index]
+  if selectedItem then
+    local selectedData = widget.getData(string.format("%s.%s", itemList, selectedItem))
+    local item = self.upgradeableItems[selectedData.index]
 
-    if upgradeItem then
-      local consumedItem = player.consumeItem(upgradeItem, false, true)
+    if item then
+      local consumedItem = player.consumeItem(item, false, true)
+			
       if consumedItem then
-        local consumedCurrency = player.consumeCurrency("essence", selectedData.price)
-        local upgradedItem = copy(consumedItem)
-        if consumedCurrency then
-          upgradedItem.parameters.level = self.upgradeLevel
-          local itemConfig = root.itemConfig(upgradedItem)
-          if itemConfig.config.upgradeParameters then
-            upgradedItem.parameters = util.mergeTable(upgradedItem.parameters, itemConfig.config.upgradeParameters)
-          end
-        end
-        player.giveItem(upgradedItem)
+        local consumedUpgradeItem = player.consumeItem({name = upgradeItem, count = selectedData.price}, false, false)
+				
+        if consumedUpgradeItem then
+					local consumedConfig = root.itemConfig(consumedItem).config
+					local newItem = consumedConfig.upgradeItem
+					if type(newItem) == "string" then
+						newItem = {name = newItem}
+					end
+					player.giveItem(newItem)
+        else
+					player.giveItem(consumedItem)
+				end
       end
     end
+		
     populateItemList(true)
   end
 end
